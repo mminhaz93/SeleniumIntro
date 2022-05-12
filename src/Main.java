dd6ad04144b9dbb4411573b995ea4dafb68620d5
mminhaz <mminhaz@ibm.com>
2022-05-11 20:06:48 -0400

    navigation

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,22 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
                 +"/chromedriver");
 
-
         // Creates an instance of WebDriver using ChromeDriver
         // Telling selenium to use chrome
         WebDriver driver = new ChromeDriver();
 
         // opens browser to certain url
-        driver.get("https://www.mztech.us");
+        driver.get("https://www.mztech.us"); // .get() - waits for the entire page to load
+
+        //Navigation commands
+        driver.navigate().to("https://www.facebook.com"); // this just navigates to the browser
+        driver.navigate().back(); // goes back to previous navigated url
+        driver.navigate().forward(); // goes forward to previous navigated url
+        driver.navigate().refresh(); // Refreshes the page
+
+        //Close commands
+        driver.quit(); // Closes the entire browser
+        driver.close(); // Closes the current tab or closes the browser if only one tab is open
 
         // Tells java to wait for 5 second
         Thread.sleep(5000);
